da2b4fed840044d32419dad77c91d568f205efb0
styrocupman <styrocupman@gmail.com>
2014-07-21 13:58:53 -0600

    Initial version

---
+++ STGCodeChallenge9/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace STGCodeChallenge9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(83, 62);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(111, 23);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "Check Text";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Potential Palindrome Text:";
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(16, 34);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(254, 22);
+            this.txtText.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 106);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.Text = "Palindrome Check";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtText;
+    }
+}
+
---
+++ STGCodeChallenge9/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace STGCodeChallenge9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string s = txtText.Text.Trim();
+            MessageBox.Show(string.Format("The string '" + s + "' is{0} a palindrome.", (isPalindrome(s) ? "" : " not")));
+        }
+
+        private bool isPalindrome(string s)
+        {
+            long l;
+            char[] arr;
+
+            // If the input is one number, do the binary check
+            if (long.TryParse(s, out l))
+            {
+                s = Convert.ToString(l, 2);
+            }
+
+            // See if we have a palindrome
+            // Convert all the chars to lowercase and remove non-alphanumerics
+            arr = s.ToLowerInvariant().Where(c => (char.IsLetterOrDigit(c))).ToArray();
+            s = new string(arr);
+
+            // Split the string in two - 1st half and 2nd half
+            string s1, s2;
+            int len = s.Length / 2;
+            s1 = s.Substring(0, len);
+            s2 = s.Substring((s.Length % 2 != 0 ? len + 1 : len), len);
+            // Reverse the 2nd half
+            arr = s2.ToCharArray();
+            Array.Reverse(arr);
+            s2 = new string (arr);
+
+            // If the two halves are the same we have a palindrome
+            return (s1 == s2);
+        }
+    }
+}
